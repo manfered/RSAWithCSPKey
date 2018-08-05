6f4bf2d0c9674a46148135ce7e9e5c2962ea296e
manfered <35757736+manfered@users.noreply.github.com>
2018-08-05 17:53:06 -0600

    Add project files.

---
+++ RSAWithCSPKey/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAWithCSPKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rsaCsp = new RSAWithCSPKey();
+            const string message = "Message tp Encrypt";
+
+            rsaCsp.AssignNewKey();
+
+            var encrypted = rsaCsp.EncryptData(Encoding.UTF8.GetBytes(message));
+            var decrypted = rsaCsp.DecryptData(encrypted);
+
+            rsaCsp.DeleteKeyInCSP();
+
+            Console.WriteLine("RSA CSP Based Key");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Message text : {message}");
+            Console.WriteLine($"Encrypted data : {Convert.ToBase64String(encrypted)}");
+            Console.WriteLine($"Decrypted data : {Encoding.Default.GetString(decrypted)}");
+        }
+    }
+}
---
+++ RSAWithCSPKey/RSAWithCSPKey.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace RSAWithCSPKey
+{
+    public class RSAWithCSPKey
+    {
+        const string ContainerName = "FarzadContainer";
+
+        public void AssignNewKey()
+        {
+            CspParameters cspParams = new CspParameters(1);
+            cspParams.KeyContainerName = ContainerName;
+            cspParams.Flags = CspProviderFlags.UseMachineKeyStore;
+            cspParams.ProviderName = "Microsoft Strong Cryptographic Provider";
+
+            var rsa = new RSACryptoServiceProvider(cspParams) { PersistKeyInCsp = true };
+        }
+
+        public void DeleteKeyInCSP()
+        {
+            var cspParams = new CspParameters { KeyContainerName = ContainerName };
+            var rsa = new RSACryptoServiceProvider(cspParams) { PersistKeyInCsp = false };
+
+            rsa.Clear();
+        }
+
+        public byte[] EncryptData(byte[] dataToEncrypt)
+        {
+            byte[] cipherData;
+
+            var cspParams = new CspParameters { KeyContainerName = ContainerName };
+
+            using (var rsa = new RSACryptoServiceProvider(2048, cspParams))
+            {
+                cipherData = rsa.Encrypt(dataToEncrypt, false);
+            }
+
+            return cipherData;
+        }
+
+        public byte[] DecryptData(byte[] dataToDecrypt)
+        {
+            byte[] plain;
+
+            var cspParams = new CspParameters { KeyContainerName = ContainerName };
+
+            using (var rsa = new RSACryptoServiceProvider(2048, cspParams))
+            {
+                plain = rsa.Decrypt(dataToDecrypt, false);
+            }
+
+            return plain;
+        }
+    }
+}
